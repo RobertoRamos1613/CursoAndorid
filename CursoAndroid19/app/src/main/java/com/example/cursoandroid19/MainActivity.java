8de8b19c6415aa275b90dbb655f2bac75b6fc535
RobertoRamos1613 <rur_ramos@hotmail.com>
2019-11-02 14:48:13 -0600

    Se hizo una vinculacion entre dos clases

--- CursoAndroid19/app/src/main/java/com/example/cursoandroid19/MainActivity.java
+++ CursoAndroid19/app/src/main/java/com/example/cursoandroid19/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.cursoandroid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Hola desde onCreate",Toast.LENGTH_SHORT).show();
+
+        boton = (Button) findViewById(R.id.Boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"El empalador",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Hola desde onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Hola desde onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Hola desde onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Hola desde onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Hola desde onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Hola desde onRestart",Toast.LENGTH_SHORT).show();
+    }
+
 }
